8f285db31fed6d72d33d9490a992f55fdf9cc1cd
Keresifon <keresifon@gmail.com>
2020-07-23 22:37:21 +0100

    delete

--- src/component/product/Cart.js
+++ src/component/product/Cart.js
@@ -1,46 +1,73 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { CartContext } from '../../CartContext';
-import { Container, Col, Row,  Image } from 'react-bootstrap';
+import { Container, Col, Row, Image, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 function Cart() {
 	const [cart, setCart] = useContext(CartContext);
- 
-    const totalPrice = cart.reduce((acc, curr) => acc + curr.price*curr.qty , 0)
+	const [qty, setQty] = useState(1);
 
+	const totalPrice = cart.reduce((acc, curr) => acc + curr.price * curr.qty, 0);
+
+	const removeFromCart = (item) => {
+		const ncart = cart.filter((cartItem) => cartItem._id !== item._id);
+		setCart(ncart);
+	};
 
 	return (
 		<>
-		<div className = "container">
-			<div className = "row border-bottom ">
-            <div className = "h4 font-weight-bold     pt-5 col-md-4 mr-2  ">Shopping Cart</div>
-            <div className=" col-md-4  pt-5 ml-auto  d-flex pb-4  justify-content-end" > Price</div>
-			</div>
-			<div className="container-fluid">
-			
-			
-           <div>
-
-		  
-		   <Container  fluid>
-			   
-   {cart.map(item=>
-   
-  <Row className="p-3 border-bottom" key={item._id}>
-    <Col sm={3}><Image src={item.image} alt="" className="img-fluid ml-6 " /></Col>
-	<Col><div><Link to={`/products/${item._id}`}>{item.name}</Link></div>
-   <div>{item.qty}</div>
-	</Col>
-    <Col className="d-flex justify-content-end">${item.price*item.qty}</Col>
-    
-  </Row>
-   )}
-</Container>
-		   </div>
-		</div>
-			
-        </div>
-		<div className=" container   pt-5 ml-auto  d-flex pb-4  justify-content-end h4" > Total : ${totalPrice} </div>
+			<div className="container">
+				<div className="row border-bottom ">
+					<div className="h4 font-weight-bold     pt-5 col-md-4 mr-2  ">Shopping Cart</div>
+					<div className=" col-md-4  pt-5 ml-auto  d-flex pb-4  justify-content-end"> Price</div>
+				</div>
+				<div className="container-fluid">
+					<div>
+						{cart.length === 0 ? (
+							<div> This is an empty Cart </div>
+						) : (
+							<Container fluid>
+								{cart.map((item) => (
+									<Row className="p-3 border-bottom" key={item._id}>
+										<Col sm={3}>
+											<Image src={item.image} alt="" className="img-fluid ml-6 " />
+										</Col>
+										<Col>
+											<div>
+												<Link to={`/products/${item._id}`}>{item.name}</Link>
+											</div>
+											<div>
+												Qty:
+												<select
+													className=" custom-select-sm"
+													value={item.qty}
+													onChange={(e) => {
+														setQty(e.target.value);
+													}}
+												>
+													<option>1</option>
+													<option>2</option>
+													<option>3</option>
+													<option>4</option>
+												</select>
+												{item.qty}
+											</div>{' '}
+											<Button onClick={() => removeFromCart(item)}>Delete</Button>
+										</Col>
+										<Col className="d-flex justify-content-end">${item.price * item.qty}</Col>
+									</Row>
+								))}
+							</Container>
+						)}
+					</div>
+				</div>
+			</div>{' '}
+			{cart.length > 0 && (
+				<div className=" container   pt-5 ml-auto  d-flex pb-4  justify-content-end h4">
+					{' '}
+					Total : ${totalPrice}{' '}
+				</div>
+			)}
 		</>
 	);
 }
--- src/component/product/Trending.js
+++ src/component/product/Trending.js
@@ -1,17 +1,13 @@
-import React, {useState, useEffect, useContext } from 'react';
-import { Card , Button} from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import http from '../../services/httpService'
+import React, { useState, useEffect, useContext } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import http from '../../services/httpService';
 import { CartContext } from '../../CartContext';
-import { ShoppingBasket} from '../../services/cartService'
-
-
-
-
+import { ShoppingBasket } from '../../services/cartService';
 
 function Trending(props) {
-  const [products, setProduct] = useState([]);
-  const [cart, setCart] = useContext(CartContext);
+	const [products, setProduct] = useState([]);
+	const [cart, setCart] = useContext(CartContext);
 	const [qty, setQty] = useState(1);
 
 	useEffect(() => {
@@ -21,53 +17,43 @@ function Trending(props) {
 		};
 		fetchData();
 		//return () => {};
-  }, []);
+	}, []);
 
-  const  addToCart =  ShoppingBasket (cart, qty, setCart);
-  
+	const addToCart = ShoppingBasket(cart, qty, setCart);
 
-  const trending = products.filter(p => p.trending === true)
-  
-    return (
-      <>
-     <div>
-     
-        <div className="container md-5 " >
-            <div className="row">
-            { trending.map(product => 
-        <div className="col-sm-3" key={product._id}>
-        
-        <Card className="border-faded-warning rounded-lg">
-        <Link to={`/products/${product._id}`}>
-  <Card.Img variant="top" src={product.image} /> 
-  </Link>
-  <Card.Body>
-    <Card.Title><Link to={`/products/${product._id}`}>{product.name}</Link></Card.Title>
-    <Card.Text>
-      ${product.price}
-    </Card.Text>
-    <Card.Text className="text-small">
-    <Link to={`/product/${product.category}`}>
-      {product.category}
-      </Link>
-    </Card.Text>
-    <Button variant="outline-primary" onClick={() => addToCart(product)}>Add To Cart  </Button>
-  </Card.Body>
-</Card>   
+	const trending = products.filter((p) => p.trending === true);
 
-</div>
-
-
-)}
-       
-        </div>
-        </div>
-        
-        </div>
-       
-        </>
-    );
-   
+	return (
+		<>
+			<div>
+				<div className="container md-5 ">
+					<div className="row">
+						{trending.map((product) => (
+							<div className="col-sm-3" key={product._id}>
+								<Card className="border-faded-warning rounded-lg">
+									<Link to={`/products/${product._id}`}>
+										<Card.Img variant="top" src={product.image} />
+									</Link>
+									<Card.Body>
+										<Card.Title>
+											<Link to={`/products/${product._id}`}>{product.name}</Link>
+										</Card.Title>
+										<Card.Text>${product.price}</Card.Text>
+										<Card.Text className="text-small">
+											<Link to={`/product/${product.category}`}>{product.category}</Link>
+										</Card.Text>
+										<Button variant="outline-primary" onClick={() => addToCart(product)}>
+											Add To Cart{' '}
+										</Button>
+									</Card.Body>
+								</Card>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Trending;
