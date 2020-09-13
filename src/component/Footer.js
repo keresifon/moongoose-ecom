375e832bf236dce577bb881a70cf0cce76e1c7f6
Keresifon <keresifon@gmail.com>
2020-09-13 13:36:08 +0100

    Update Footer.js

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -7,7 +7,7 @@ function Footer(props) {
    <div  >
     <Navbar bg="dark" variant="dark"   className="footer" >
     <Nav className="mr-auto">
-    <Nav.Link>Copyright 2020 NoWahala.com</Nav.Link>
+    <Nav.Link>Copyright 2020 Okpogho.com</Nav.Link>
     </Nav>
     <Nav className="ml-auto">
       <Nav.Link as={Link} to="/">Home</Nav.Link>
